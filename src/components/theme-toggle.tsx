68d93cf96a7e6fe4bb0f0b3ddd354a16e8e362c3
Yomal theekshana <yomaltheekshana66@gmail.com>
2025-09-03 20:31:24 +0000

    smooth that button change

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -18,8 +18,8 @@ export function ThemeToggle() {
       thumbClassName="h-6 w-6 data-[state=checked]:translate-x-6 bg-white flex items-center justify-center text-black"
       aria-label="Toggle theme"
     >
-        <Sun className="h-4 w-4 transition-transform duration-500 scale-100 dark:scale-0" />
-        <Moon className="absolute h-4 w-4 transition-transform duration-500 scale-0 dark:scale-100" />
+        <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Switch>
   )
 }
